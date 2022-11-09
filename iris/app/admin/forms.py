de418d8d0c09f861c445adb02b6a58e7e793160e
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2022-11-10 00:56:42 +0100

    Populate now as default value for cancel work datetime field

--- iris/app/admin/forms.py
+++ iris/app/admin/forms.py
@@ -1,10 +1,11 @@
 from django import forms
+from django.utils.timezone import now
 
 
-class BrowserDateInput(forms.DateInput):
+class BrowserDateInput(forms.DateTimeInput):
     input_type = "datetime-local"
 
 
 class CancelWorksViewForm(forms.Form):
     reason = forms.CharField()
-    datetime = forms.DateTimeField(widget=BrowserDateInput(), required=False)
+    datetime = forms.DateTimeField(widget=BrowserDateInput(), initial=now)
