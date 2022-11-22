668cd975f62bfe024f161692247ba539feb810b1
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2022-11-22 13:46:42 +0100

    Add verbose name for iris_wc app

--- deps/iris_wc/iris_wc/apps.py
+++ deps/iris_wc/iris_wc/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class IrisWcAppConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
     name = "iris_wc"
+    verbose_name = "Iris WooCommerce"
