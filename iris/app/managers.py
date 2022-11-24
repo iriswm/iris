b7e381fd290413791b6a376836f3b7c52c803325
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2022-11-24 05:50:52 +0100

    Don't show jobs for canceled works in stations

--- iris/app/managers.py
+++ iris/app/managers.py
@@ -8,7 +8,7 @@ class JobManager(Manager):
         if station is not None:
             queryset = queryset.filter(task__stations=station)
         return (
-            queryset.filter(commit__isnull=True)
+            queryset.filter(commit__isnull=True, work__cancel_time__isnull=True)
             .annotate(
                 delays_limit=F("delays__created") + F("delays__duration"),
             )
