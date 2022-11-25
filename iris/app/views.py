697bb3da15b92442cdf53aeb5b350831c3e870f0
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2022-11-25 05:11:52 +0100

    Simple add commit view

--- iris/app/views.py
+++ iris/app/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
-from django.views.generic.base import TemplateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, DetailView, TemplateView
 
-from iris.app.models import Commit, Delay, Job, Station, Suspension
+from iris.app.models import Commit, Delay, Job, Station, Suspension, Worker
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
@@ -54,9 +53,61 @@ class AlertsView(SomePermissionRequiredMixin, TemplateView):
 class JobDetailView(DetailView):
     model = Job
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            station_pk = self.request.GET["station"]
+            _station = Station.objects.get(pk=station_pk)
+        except KeyError:
+            pass
+        except Station.DoesNotExist:
+            pass
+        else:
+            context["station_pk"] = station_pk
+        return context
 
-class CreateCommitView(CreateView):
+
+class JobItemCreateViewMixin(LoginRequiredMixin, CreateView):
+    def get_job_and_worker(self):
+        job_pk = self.request.GET["job"]
+        job = Job.objects.get(pk=job_pk)
+        worker = Worker.objects.get(user=self.request.user)
+        return job, worker
+
+    def get(self, *args, **kwargs):
+        try:
+            self.get_job_and_worker()
+        except KeyError:
+            messages.error(self.request, _("A job is required in this view."))
+            return HttpResponseRedirect(reverse("iris:index"))
+        except Job.DoesNotExist:
+            messages.error(self.request, _("An existing job is required in this view."))
+            return HttpResponseRedirect(reverse("iris:index"))
+        return super().get(*args, **kwargs)
+
+    def form_valid(self, form):
+        job, worker = self.get_job_and_worker()
+        form.instance.job = job
+        form.instance.worker = worker
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        success_url = reverse("iris:index")
+        try:
+            station_pk = self.request.GET["station"]
+            _station = Station.objects.get(pk=station_pk)
+        except KeyError:
+            pass
+        except Station.DoesNotExist:
+            pass
+        else:
+            success_url = reverse("iris:station", args=[station_pk])
+        return success_url
+
+
+class CreateCommitView(JobItemCreateViewMixin, CreateView):
     model = Commit
+    fields = ["notes"]
 
 
 class CreateDelayView(CreateView):
